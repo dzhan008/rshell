172c85184138bc875d2d71cda7a1320e141bb3c0
dzhan008 <dzhan008@ucr.edu>
2016-02-10 02:15:43 -0800

    Started on parsing algorithm.
    So far the algorithm can parse through the tokenized text through
    spaces. I need it to parse through strings and break them apart if a
    connector exists in it.

--- shell.cpp
+++ shell.cpp
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <vector>
+#include <pwd.h>
 
 using namespace std;
 using namespace boost;
@@ -15,17 +16,62 @@ class Shell {
     public:
         Shell() {};
         ~Shell() {};
-        void parse(string parsley) 
+        void run() 
         {
-            typedef tokenizer<escaped_list_separator<char> > tokenizer;
-            escaped_list_separator<char> sep("", "||&&;", "");
-            tokenizer tokens(parsley, sep);
-            int i = 0;
-            for (tokenizer::iterator it = tokens.begin(); it != tokens.end(); ++it, ++i)
+            
+            char * name;
+            char hostname[1024];
+            gethostname(hostname, 1024);
+            struct passwd* pass;
+            pass = getpwuid(getuid());
+            name = pass->pw_name;
+               
+            while(command != "exit") 
             {
-                cout << *it << endl;
-                //v.push_back(*it);
+                cout << name << "@" << hostname << "$ ";
+                getline(cin, command);
+                cout << endl;
+                parse(command);
             }
+        }
+        // Note that we cannot distinguish between "|" and "||" so if a user typed "|" on accident parsing would not catch it.
+        // ie. char_separator<char> sep2("|"); == char_separator<char> sep2("||");
+        void parse(string cmdLine) 
+        {
+            //Also cmdLine does not get updated each time we parse. Forgot about that.
+            
+            // typedef tokenizer<char_separator<char> > tokenizer1;
+            // char_separator<char> sep1(" ");
+            // tokenizer1 tokens1(cmdLine, sep1);
+            
+            //Parses by each command
+            typedef tokenizer<char_separator<char> > tokenizer;
+            char_separator<char> sep(" ");
+            tokenizer tokens(cmdLine, sep);
+            vector<string> v;
+            string test;
+            for (tokenizer::iterator it = tokens.begin(); it != tokens.end(); ++it)
+            {
+                if(*it == "||" || *it == "&&" || *it == ";")
+                {
+                    std::cout << "Connector detected!" << std::endl;
+                    v.push_back(test);
+                    test = "";
+                    v.push_back(*it);
+
+                }
+                else
+                {
+                    test = test + *it + " ";
+                }
+                //cout << *it;
+            }
+            
+            for (int i = 0; i < v.size(); ++i)
+            {
+                cout << v.at(i) << endl;
+            }
+            //cout << endl;
         }
         
         /*
@@ -38,64 +84,49 @@ class Shell {
         
         However, we may have a problem with this. Since we're splitting these up into different strings with delimiters, we need to find a way to keep the white space.
         We can keep it using Kept Limiters.
-        
-        
-        
         */
         
-/*        bool execute(vector<string> v)
-        {
-            for(i = 0; i < v.size(); ++i)
-            {
-                if(v.at(i) == "||")
-                {
+        // bool execute(vector<string> v)
+        // {
+        //     for(int i = 0; i < v.size(); ++i)
+        //     {
+        //         if(v.at(i) == "||")
+        //         {
                     
-                }
-                else if(v.at(i) == "&&")
-                {
+        //         }
+        //         else if(v.at(i) == "&&")
+        //         {
                     
-                }
-                else if(v.at(i) == ";")
-                {
+        //         }
+        //         else if(v.at(i) == ";")
+        //         {
                     
-                }
-                else
-                {
+        //         }
+        //         else
+        //         {
                 
-                ls -a || cd blah
-                    char* temp[2];
-                    temp[0] = (char*)v.at(i).c_str();
-                    temp[1] = NULL;
-                    pit_t pid = fork();
-                    if (pid == 0)
-                    {
-                        if(execvp(temp[0], temp) == -1)
-                        {
-                            perror ("exec");
-                            return false;
-                        }
-                    }
-                    if (pid > 0)
-                    {
-                        if (wait(0) == -1)
-                        {
-                            perror("wait");
-                        }
-                    }
+        //         }
+        //         char* temp[2];
+        //         temp[0] = (char*)v.at(i).c_str();
+        //         temp[1] = NULL;
+        //         pit_t pid = fork();
+        //         if (pid == 0)
+        //         {
+        //             if(execvp(temp[0], temp) == -1)
+        //             {
+        //                 perror ("exec");
+        //                 return false;
+        //             }
+        //         }
+        //         if (pid > 0)
+        //         {
+        //             if (wait(0) == -1)
+        //             {
+        //                 perror("wait");
+        //             }
+        //         }
 
-                }
-            }
-            return true;
-        }*/
-        void run() 
-        {
-            while(command != "exit") 
-            {
-                cout << "$ ";
-                getline(cin, command);
-                cout << endl;
-                parse(command);
-
-            }
-        }
-};
+        //     }
+        // }
+        // return true;
+    };
