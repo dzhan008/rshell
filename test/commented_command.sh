28dc352439a84ce10a617b51c2008ded3e154dac
Michaella Sheng <mshen010@hammer.cs.ucr.edu>
2016-02-12 21:45:05 -0800

    Added more test case shell scripts.

--- test/commented_command.sh
+++ test/commented_command.sh
@@ -1,4 +1,12 @@
-echo "User enters comment."
-#this is a comment
-echo "User enters comment "
+echo "User enters ls followed by a comment."
 ls #this is a comment
+echo "User enters comment with command inside"
+# ls
+echo "User enters command with connector followed by comment."
+ls || #pwd
+echo "User enters command with connector followed by comment."
+ls && #pwd
+echo "User enters command with connector followed by comment."
+ls; #pwd
+echo "User enters command with a comment pound sign multiple times"
+pwd ##comment##comment 
---
+++ test/exit.sh
@@ -0,0 +1,18 @@
+echo "User enters exit"
+exit
+echo "User enters <command> || exit"
+pwd || exit
+echo "User enters failed command || exit"
+ped || exit
+echo "User enters <command> && exit"
+pwd && exit
+echo "User enters failed command && exit"
+ped && exit
+echo "User enters <command> ; exit"
+pwd; exit
+echo "User enters failed command ; exit"
+pew ; exit
+echo "User enters exit multiple times"
+exit exit
+echo "User enters exit followed by gibberish"
+exit afdsf
