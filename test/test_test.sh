daf4e9b735001a0536f31b7280f6c55f2bc12093
mshen010 <mshen010@ucr.edu>
2016-03-06 19:41:51 -0800

    Added more test cases and fixed some

--- test/test_test.sh
+++ test/test_test.sh
@@ -1,26 +1,34 @@
 # Test if the file path exists
+test -e /bin
+    # returns true
+test -e bin
+    # returns true
+test -e bin/main.cpp
+    # returns true
+test -e main.cpp/bin
+    # returns false   
 test -e main.cpp
-	# returns true
+	# returns false
 test -e /main.cpp
 	# returns false
 [ -e main.cpp ]
-	# returns true
+	# returns false
 [ -e /main.cpp ]
 	# returns false
 
 # Tests if file path exists and is a file
 test -f main.cpp
-	# returns true
-test -f /main.cpp
-    # returns false
+	# returns false
 test -f /bin
 	# returns false
+test -f /bin/main.cpp
+    # returns true
 [ -f main.cpp ]
-	# return true
-[ -f /main.cpp ]
 	# return false
 [ -f /bin.cpp ]
-	# returns false
+	# return false
+[ -f bin/main.cpp ]
+	# returns true
 
 # Tests if file path exists and is a directory
 test -d /bin
@@ -36,7 +44,17 @@ test -d main.cpp
 [ -d main.cpp ]
 	# returns false
 	
-# Tests for errors
+# Tests test call without specifying flag
+test main.cpp
+    # returns false
+test bin/main.cpp
+    # returns true
+[ main.cpp ]
+    # returns false
+[ bin/main.cpp ]
+    # returns true
+    
+# Tests for errors in syntax
 testt -e /main.cpp
 	# throws error with 'test'
 test -q /main.cpp
